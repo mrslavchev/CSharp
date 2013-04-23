4ea7d3293f0cb0d361b3fb680462fb84918b5aef
Viktor Slavchev <mr.slavchev@gmail.com>
2013-04-23 21:17:41 +0300

    .

---
+++ 05.Conditional statements/05.Conditional statements/Conditional statements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Conditional_statements
+{
+    class Conditional statements
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 05.Conditional statements/05.Conditional statements/statements.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05.Conditional_statements
+{
+    class statements
+    {
+    }
+}
