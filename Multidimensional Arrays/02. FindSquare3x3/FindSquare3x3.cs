9b41837dd09d03a807c4bae4a183f2f5f16ae8d8
Viktor Slavchev <mr.slavchev@gmail.com>
2013-07-20 21:52:40 +0300

    FindScuqare commit

--- Multidimensional Arrays/02. FindSquare3x3/FindSquare3x3.cs
+++ Multidimensional Arrays/02. FindSquare3x3/FindSquare3x3.cs
@@ -1,56 +1,141 @@
 ﻿using System;
+//Write a program that reads a rectangular matrix of size N x M and finds in it the square 3 x 3 that has maximal sum of its elements.
 
-
-    class FindSquare3x3
+/// <summary>
+/// Contains separate methods for reading the matrix, printing it, scan the matrix for 
+/// the 3x3 square, sum the square itself, and print the result
+/// </summary>
+public class FindSquare3x3
+{
+    /// <summary>
+    /// Reads matrix from the console
+    /// </summary>
+    /// <returns>2D array</returns>
+    public static int[,] ReadMatrix()
     {
-        static int[,] matrix;
 
-        static void Main(string[] args)
+        Console.WriteLine("Please enter row size: ");
+        int rows = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter col size: ");
+        int cols = int.Parse(Console.ReadLine());
+        if (rows < 3 || cols < 3)
         {
-            //Write a program that reads a rectangular matrix of size N x M and finds in it the square 3 x 3 that has maximal sum of its elements.
-            int row = 0,
-                col = 0;
-            row = int.Parse(Console.ReadLine());
-            col = int.Parse(Console.ReadLine());
-            matrix = new int[row, col];
-
-            for (int i = 0; i < row; i++)
-            {
-                for (int j = 0; j < col; j++)
-                {
-                    matrix[i, j] = int.Parse(Console.ReadLine());
-                }
-            }
-
-            int currentSum = 0;
-            int maxSum = int.MinValue;
-
-            for (int i = 0; i <= row - 3; i++)
-            {
-                for (int j = 0; j <= col - 3; j++)
-                {
-                    currentSum = SumInSquare(i, j);
-                    if (currentSum > maxSum)
-                    {
-                        maxSum = currentSum;
-                    }
-                }
-            }
-            Console.WriteLine(maxSum);
+            Console.WriteLine("Wrong input each side should be at least 3");
         }
 
-        static int SumInSquare(int rowIndex, int colIndex)
+        int[,] matrix = new int[rows, cols];
+        for (int i = 0; i < rows; i++)
         {
-            int result = 0;
-
-            for (int i = rowIndex; i < 3 + rowIndex; i++)
+            Console.WriteLine("Type values for row {0}", i);
+            for (int k = 0; k < cols; k++)
             {
-                for (int j = colIndex; j < 3 + colIndex; j++)
-                {
-                    result += matrix[i, j];
-                }
+                matrix[i, k] = int.Parse(Console.ReadLine());
             }
+        }
+        return matrix;
 
-            return result;
+
+    }
+
+    /// <summary>
+    /// Prints array to the console
+    /// </summary>
+    /// <param name="array">Takes 2D array as single parameter</param>
+    public static void PrintMatrix(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                Console.Write(Convert.ToString(array[i, k]).PadLeft(3, ' '));
+            }
+            Console.WriteLine();
         }
     }
+
+    /// <summary>
+    /// Calculates a sum for a 3x3 suqare
+    /// within a 2D array
+    /// </summary>
+    /// <param name="array">Takes 2D array as first parameter</param>
+    /// <param name="rowIndex">Takes row coordinate as second parameter</param>
+    /// <param name="colIndex">Takes column coordinate as third parameter</param>  
+    public static int GetSum(int[,] array, int rowIndex, int colIndex) 
+    {
+        int result = 0;
+        for (int i = rowIndex; i < rowIndex + 3; i++)
+        {
+            result = result + array[i, colIndex];
+            for (int k = colIndex; k < colIndex + 2; k++)
+            {
+                result = result + array[i, k + 1];
+            }
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Scans a matrix using the GetSum method
+    /// By incrementing row and cow, we are moving the 3x3 square all over the matrix
+    /// </summary>
+    /// <param name="matrix">2d array</param>
+    private static void ScanMatrix(int[,] matrix, out int bestSum, out int[] bestCoords)
+    {
+        int sum = 0;
+        bestSum = int.MinValue;
+        bestCoords = new int[2];
+        for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+            {
+                sum = GetSum(matrix, row, col);
+                if (sum > bestSum)
+                {
+                    bestSum = sum;
+                    bestCoords[0] = row;
+                    bestCoords[1] = col;
+                    sum = 0;
+                }
+                else
+                {
+                    sum = 0;
+                }
+            }
+        }
+
+        PrintBestResutlt(matrix, bestCoords[0], bestCoords[1]);
+    }
+
+    /// <summary>
+    /// Prints the best result found
+    /// </summary>
+    /// <param name="array">2d array</param>
+    /// <param name="rowCor">Row coordinate</param>
+    /// <param name="colCor">Column coordinate</param>
+    public static void PrintBestResutlt(int[,] array, int rowCor, int colCor) 
+    {
+        for (int i = rowCor; i < rowCor + 3; i++)
+        {
+            for (int k = colCor; k < colCor + 3; k++)
+            {
+                Console.Write(Convert.ToString(array[i, k]).PadLeft(3, ' '));
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        int[,] inputArray = ReadMatrix();
+        int bestSum =0;
+        int[] bestCoords;
+        Console.WriteLine("The matrix is: ");
+        PrintMatrix(inputArray);
+        Console.WriteLine("The best square is:");
+        ScanMatrix(inputArray, out bestSum, out bestCoords);
+        Console.WriteLine("The best sum is :" + bestSum);
+    }
+
+    
+}
