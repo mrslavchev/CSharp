86cf81a6112f29000986c6c41dd7fc8055520b6c
Viktor Slavchev <mr.slavchev@gmail.com>
2013-08-22 23:38:19 +0300

    Text files last corrections

--- Text Files/12. DeleteList/DeleteList.cs
+++ Text Files/12. DeleteList/DeleteList.cs
@@ -2,44 +2,104 @@
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Security;
-
+using System.Collections.Generic;
+/*Write a program that removes from a text file all words listed in given another text file. 
+ * Handle all possible exceptions in your methods.
+*/
 class DeleteList
 {
+    /// <summary>
+    /// Reads file as a list of strings
+    /// </summary>
+    /// <returns>List of the words to be removed</returns>
+    static List<string> ReadFileAsList() 
+    {
+        List<string> words = new List<string>();
+        string line;
+        using (StreamReader reader = new StreamReader("../../ListToRemove.txt"))
+        {
+            while ((line = reader.ReadLine()) != null)
+            {
+                words.Add(line);
+            }
+
+            return words;
+        }
+    }
+
+    /// <summary>
+    /// Reads a file as a string
+    /// </summary>
+    /// <returns>String with file content</returns>
+    static string ReadFileAsSrting() 
+    {
+        using (StreamReader reader = new StreamReader("../../File.txt"))
+        {
+            string file = reader.ReadToEnd();
+            return file;
+        }
+    }
+
+    /// <summary>
+    /// Repmoves given list of strings from a string
+    /// </summary>
+    /// <param name="words">Takes list of strings as first parameter</param>
+    /// <param name="text">Takes string as second parameter</param>
+    /// <returns>String</returns>
+    static string RemoveList(List<string> words, string text) 
+    {
+        foreach (var word in words)
+        {
+            text = Regex.Replace(text, word, String.Empty);
+        }
+
+        return text;
+    }
+
+    /// <summary>
+    /// Writes result back to file
+    /// </summary>
+    /// <param name="input">Takes string as single parameter</param>
+    static void WriteToFile(string input ) 
+    {
+        using (StreamWriter writer = new StreamWriter("../../File.txt"))
+        {
+            writer.Write(input);
+        }
+    }
+
     static void Main()
     {
         try
         {
-            string regex = @"\b(" + String.Join("|", File.ReadAllLines("../../IpsumLorem.txt")) + @")\b";
-
-            using (StreamReader input = new StreamReader("../../LoremIpsum.txt"))
-            using (StreamWriter output = new StreamWriter("../../outputFile.txt"))
-                for (string line; (line = input.ReadLine()) != null; )
-                    output.WriteLine(Regex.Replace(line, regex, String.Empty));
+            List<string> forbiiddenWords = ReadFileAsList();
+            string file = ReadFileAsSrting();
+            string output = RemoveList(forbiiddenWords, file);
+            WriteToFile(output);
         }
-
-        catch (FileNotFoundException e)
+        catch (System.IO.FileNotFoundException)
         {
-            Console.WriteLine(e.Message);
+            Console.Error.WriteLine("Cannot Find filepath");
         }
-
-        catch (DirectoryNotFoundException e)
+        catch (System.OutOfMemoryException)
         {
-            Console.WriteLine(e.Message);
+            Console.Error.WriteLine("Out of memory");
         }
-
-        catch (IOException e)
+        catch (System.FormatException)
         {
-            Console.WriteLine(e.Message);
+            Console.Error.WriteLine("Incorrect string format");
         }
-
-        catch (SecurityException e)
+        catch (DirectoryNotFoundException)
         {
-            Console.WriteLine(e.Message);
+            Console.Error.WriteLine("Directory not found");
         }
-
-        catch (UnauthorizedAccessException e)
+        catch (UnauthorizedAccessException)
         {
-            Console.WriteLine(e.Message);
+            Console.Error.WriteLine("No aceess to read/write to that file");
+        }
+        catch (ArgumentException)
+        {
+            Console.Error.WriteLine("Empty path");
         }
     }
 }
--- Text Files/13.ListOFWords/ListOFWords.cs
+++ Text Files/13.ListOFWords/ListOFWords.cs
@@ -1,55 +1,121 @@
 ﻿using System;
 using System.IO;
 using System.Text.RegularExpressions;
-using System.Security;
+using System.Collections.Generic;
+using System.Text;
+/*Write a program that reads a list of words from a file words.txt and finds how many times each 
+ * of the words is contained in another file test.txt. The result should be written in the file result.txt 
+ * and the words should be sorted by the number of their occurrences in descending order. 
+ * Handle all possible exceptions in your methods.
+*/
 
-class Program
+class ListOFWords
 {
+    /// <summary>
+    /// Reads the words from textfile and converting them to collection of strings.
+    /// </summary>
+    /// <returns>Array of strings</returns>
+    static string[] ReadList() 
+    {
+        using (StreamReader reader = new StreamReader("../../../words.txt"))
+        {
+            string line;
+            List<string> words = new List<string>();
+            while ((line = reader.ReadLine()) != null)
+            {
+                words.Add(line);
+            }
+
+            return words.ToArray();
+        }
+    }
+
+    /// <summary>
+    /// Reads a text file
+    /// </summary>
+    /// <returns>Text as string</returns>
+    static string ReadFile() 
+    {
+        using (StreamReader reader = new StreamReader("../../test.txt"))
+        {
+            string file = reader.ReadToEnd();
+            return file;
+        }
+    }
+
+    /// <summary>
+    /// Counts the word occurances, and sorts them in an array.
+    /// We sort the array, and the words using the Array.Sort()
+    /// in a relation key-value(count, word).
+    /// </summary>
+    /// <param name="collection">List of words to ocunt</param>
+    /// <param name="text">Text to scan for matches</param>
+    /// <returns>Sorted key + value</returns>
+    static string CountWords(string[] collection, string text) 
+    {
+        int[] occurances = new int[collection.Length];
+        for (int i = 0; i < collection.Length; i++)
+        {
+            string pattern = collection[i];
+            MatchCollection matches = Regex.Matches(text, pattern);
+            occurances[i] = matches.Count;
+        }
+
+        Array.Sort(occurances, collection);
+        StringBuilder output = new StringBuilder();
+
+        for (int i = occurances.Length-1; i >= 0; i--)
+        {
+            output.AppendLine(occurances[i] + " " + collection[i]);
+        }
+
+        return output.ToString();
+    }
+    
+    /// <summary>
+    /// Writes the result to text file
+    /// </summary>
+    /// <param name="input">String to be written</param>
+    static void WriteResult(string input) 
+    {
+        using (StreamWriter writer = new StreamWriter("../../result.txt"))
+        {
+            writer.Write(input);
+        }
+    }
+
     static void Main()
     {
         try
         {
-            string[] words = File.ReadAllLines("../../words.txt"); // See exercise 6 for implementation
-            int[] values = new int[words.Length];
-
-            // Count words
-            using (StreamReader input = new StreamReader("../../test.txt"))
-                for (string line; (line = input.ReadLine()) != null; )
-                    for (int i = 0; i < words.Length; i++)
-                        values[i] += Regex.Matches(line, @"\b" + words[i] + @"\b").Count;
-
-            // Sort
-            Array.Sort(values, words);
-
-            // Write output
-            using (StreamWriter output = new StreamWriter("../../result.txt"))
-                for (int i = words.Length - 1; i >= 0; i--) // Descending order
-                    output.WriteLine("{0}: {1}", words[i], values[i]);
+            string[] words = ReadList();
+            string inputText = ReadFile();
+            string result = CountWords(words, inputText);
+            WriteResult(result);
         }
-
-        catch (FileNotFoundException e)
+        catch (System.IO.FileNotFoundException)
         {
-            Console.WriteLine(e.Message);
+            Console.Error.WriteLine("Cannot Find filepath");
         }
-
-        catch (DirectoryNotFoundException e)
+        catch (System.OutOfMemoryException)
         {
-            Console.WriteLine(e.Message);
+            Console.Error.WriteLine("Out of memory");
         }
-
-        catch (IOException e)
+        catch (System.FormatException)
         {
-            Console.WriteLine(e.Message);
+            Console.Error.WriteLine("Incorrect string format");
         }
-
-        catch (SecurityException e)
+        catch (DirectoryNotFoundException)
         {
-            Console.WriteLine(e.Message);
+            Console.Error.WriteLine("Directory not found");
         }
-
-        catch (UnauthorizedAccessException e)
+        catch (UnauthorizedAccessException)
         {
-            Console.WriteLine(e.Message);
+            Console.Error.WriteLine("No aceess to read/write to that file");
+        }
+        catch (ArgumentException)
+        {
+            Console.Error.WriteLine("Empty path");
         }
     }
 }
