947ce8cd4878db429312bffab8f94937d1d19884
mrslavchev <mr.slavchev@gmail.com>
2013-11-16 00:21:19 +0400

    2012 variant 1 exam prep

---
+++ ExamPrep2012Var1/NextDate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string day = Console.ReadLine();
+        string month = Console.ReadLine();
+        string year = Console.ReadLine();
+        string format = "d.M.yyyy";
+        DateTime date = new DateTime(int.Parse(year), int.Parse(month), int.Parse(day));
+        date = date.AddDays(1);
+        Console.WriteLine(date.ToString(format));
+    }
+}
