33ec745c750c3943ef81e580eff05394e753313c
mrslavchev <mr.slavchev@gmail.com>
2013-11-22 19:23:08 +0200

    ExamPrepVar2

---
+++ C#Part1/ExamPrep/ExamPrep2012Var2/ExcellColumns/ExcellColumns.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class ExcellColumns
+{
+    private static string[] columns = 
+    {
+        "A", "B", "C", "D", "E",
+        "F", "G", "H", "I", "J",
+        "K", "L", "M", "N", "O",
+        "P", "Q", "R", "S", "T",
+        "U", "V", "W", "X", "Y",
+        "Z"
+     };
+
+    public static int FindValue(string letter)
+    {
+        return Array.BinarySearch(columns, letter) + 1; // it's a zero-based array
+    }
+
+    static void Main(string[] args)
+    {
+        long sum = 0;
+        int len = int.Parse(Console.ReadLine());
+        for (int i = 0; i < len; i++)
+        {
+            long input = FindValue(Console.ReadLine());
+            sum += input * (long)Math.Pow(26,(len - 1 - i));
+        }
+
+        Console.WriteLine(sum);
+    }
+}
+
