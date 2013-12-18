3a7731eceddcabefb8b57c585ad32673313d04c3
mrslavchev <mr.slavchev@gmail.com>
2013-12-18 09:07:06 +0200

    ExamPrepP2 - task 1

---
+++ C#Part1/ExamPrep/ExamPrepPart2/MultiverseCommunication/MultiverseCommunication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MultiverseCommunication
+{
+    public static void Main(string[] args)
+    {
+        string[] digits = new string[] { "CHU", "TEL", "OFT", "IVA", "EMY", "VNB", "POQ", "ERI", "CAD", "K-A", "IIA", "YLO", "PLA", };
+        string input = Console.ReadLine();
+        long result = 0;
+        int power = 0;
+        for (int i = input.Length - 3; i >= 0; i -= 3)
+        {
+            string digit = input.Substring(i, 3);
+            long value = Array.IndexOf(digits, digit);
+            result += value * (long)Math.Pow(13, power);
+            power++;
+        }
+        Console.WriteLine(result);
+    }
+}
+
