ff1ea7b7e905118bd88370c93a9333232f86040c
mrslavchev <mr.slavchev@gmail.com>
2014-02-18 22:41:15 +0200

    Added exception task

---
+++ C# OOP/Principles in OOP part2/03.Exception/InvalidRangeException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class InvalidRangeException<T> : Exception
+{
+    private const string Message = "Value out of range!";
+
+    public T Start { get; set; }
+    public T End { get; set; }
+
+    public InvalidRangeException(T start, T end, Exception innerException = null)
+        : base(Message, innerException)
+    {
+        this.Start = start;
+        this.End = end;
+    }
+}
+
