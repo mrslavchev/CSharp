5ee11184f8a531024ea7d659aae4e570c9585b0b
mrslavchev <mr.slavchev@gmail.com>
2014-02-26 22:30:03 +0200

    Added bitArray task

---
+++ C# OOP/Common Type System/05.BitArray/BitArrTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class BitArrTests
+{
+    static void Main()
+    {
+        BitArray visited = new BitArray(70);
+
+        Console.WriteLine("Count: " + visited.Count);
+        Console.WriteLine("Capacity: " + visited.Capacity);
+
+        visited[1] = true;
+        Console.WriteLine(visited[1]);
+
+        visited[1] = false;
+        Console.WriteLine(visited[1]);
+
+
+        Console.WriteLine();
+
+        
+        visited[0] = true;
+        visited[visited.Count - 1] = true;
+        Console.WriteLine(visited);
+        
+    }
+}
