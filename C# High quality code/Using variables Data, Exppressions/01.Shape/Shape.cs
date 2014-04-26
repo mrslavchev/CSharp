a68aad24c9feebe3db1005eef6aa29c88fee4eae
mrslavchev <mr.slavchev@gmail.com>
2014-04-27 01:07:45 +0300

    Added new homework
    Using variable and expressions.

---
+++ C# High quality code/Using variables Data, Exppressions/01.Shape/Shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Shape
+{
+    public Shape(double width, double height)
+    {
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public double Width { get; set; }
+
+    public double Height { get; set; }
+
+    public static Shape RotateShape(Shape inputShape, double rotationAngle)
+    {
+        double rotationAngleCos = Math.Abs(Math.Cos(rotationAngle));
+        double rotationAngleSin = Math.Abs(Math.Sin(rotationAngle));
+        double newShapeWidth = (rotationAngleCos * inputShape.Width) + (rotationAngleSin * inputShape.Height);
+        double newShapeHeight = (rotationAngleSin * inputShape.Width) + (rotationAngleCos * inputShape.Height);
+        Shape outputShape = new Shape(newShapeWidth, newShapeHeight);
+
+        return outputShape;
+    }
+}
---
+++ C# High quality code/Using variables Data, Exppressions/02. PrintStatistics/PrintingStatistics.cs
@@ -0,0 +1,34 @@
+﻿public class PrintingStatistics
+{
+    public void PrintStatistics(double[] inputArray, int rangeIndex)
+    {
+        double maxValue = double.MinValue;
+        double minValue = double.MaxValue;
+        double valuesSum = 0;
+        for (int i = 0; i < rangeIndex; i++)
+        {
+            if (inputArray[i] > maxValue)
+            {
+                maxValue = inputArray[i];
+            }
+            else if (inputArray[i] < minValue)
+            {
+                minValue = inputArray[i];
+            }
+
+            valuesSum += inputArray[i];
+        }
+
+        this.PrintNumber(maxValue);
+        this.PrintNumber(minValue);
+        double avarageValue = valuesSum / rangeIndex;
+        this.PrintNumber(avarageValue);
+    }
+
+    /* I strongly doubt it printing max, min or avarage differs in any manner, 
+     that's why we only leave one method.*/
+    public void PrintNumber(double input)
+    {
+        //TO DO...
+    }
+}
